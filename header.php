eb7447ca2139cabad133f7990c32a2203817c012
Vitali Maksimov <vitamak@greeny.cs.tlu.ee>
2015-10-19 21:55:25 +0300

    added files

---
+++ header.php
@@ -0,0 +1,14 @@
+<html>
+ <head>
+  <title>Hello</title>
+ </head>
+ <body>
+
+  <h1>Hello mate</h1>
+  <p>this site based on php + html</p>
+
+  <h2>well well well</h2>
+  <p>i see u neo</p>
+
+ </body>
+</html>
