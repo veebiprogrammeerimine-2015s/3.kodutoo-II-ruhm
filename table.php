04dabb151f8e68eccb64ff4664a08b711605d11e
Rasmus Kivipõld <sizen@greeny.cs.tlu.ee>
2015-10-29 19:46:22 +0200

    added table.txt, updated  table.php

--- table.php
+++ table.php
@@ -1,1 +1,28 @@
-asd
+<table border= 1>
+	<tr>
+		<th>id</th>
+		<th>username</th>
+		<th>book name</th>
+		<th>description(max 30char)</th>
+		<th>rating(0.0-5.0)</th>
+		<th>X</th>
+		<th>Edit</th>
+	</tr>
+	
+	
+	<?php
+		for($i = 0; $i < count($book_list); $i++){
+			echo "<tr>";
+			
+				echo "<td>".$book_list[$i]->id."</td>";
+				echo "<td>".$book_list[$i]->user_name."</td>";
+				echo "<td>".$book_list[$i]->book_name."</td>";
+				echo "<td>".$book_list[$i]->book_description."</td>";
+				echo "<td><a href='?delete=".$book_list[$i]->id."'>X</td>";
+				echo "<td><a href='?edit=".$book_list[$i]->id."'>Edit</td>";
+				
+			echo "</tr>"; 
+		}
+	
+	?>
+</table>
