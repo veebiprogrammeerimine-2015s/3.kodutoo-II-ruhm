46bb6c499fa5cad9394c6d3824c35d0cd67326e5
Vitali Maksimov <vitamak@greeny.cs.tlu.ee>
2015-10-30 13:01:37 +0200

    added table

--- table/1functions.php
+++ table/1functions.php
@@ -5,14 +5,14 @@
 
 	
 		//loome uue funktsiooni, et küsida ab'ist andmeid
-	function getCarData(){
+	function getKULUTUS(){
 		
 		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
-		
-		$stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates WHERE deleted IS NULL");
-		$stmt->bind_result($id, $user_id, $number_plate, $color_from_db);
+ 		
+		$stmt = $mysqli->prepare("SELECT id, name, secondname, age, eriala FROM user_register1");
+		$stmt->bind_result($id, $name, $secondname, $age, $eriala);
 		$stmt->execute();
-		
+ 		
 		// tühi massiiv kus hoiame objekte (1 rida andmeid)
 		$array = array();
 		
@@ -21,52 +21,23 @@
 		while($stmt->fetch()){
 			
 			// loon objekti iga while tsükli kord
-			$car = new StdClass();
-			$car->id = $id;
-			$car->number_plate = $number_plate;
-			$car->user_id = $user_id;
-			$car->color = $color_from_db;
+			$kul = new StdClass();
+			$kul->id = $id;
+			$kul->name = $name;
+			$kul->secondname = $second;
+			$kul->age = $age;
+			$kul->eriala = $eriala;
 			
 			// lisame selle massiivi
 			array_push($array, $car);
-			//echo "<pre>";
-			//var_dump($array);
-			//echo "</pre>";
-			
+			echo "<pre>";
+			var_dump($array);
+			echo "</pre>";
 		}
+			
+			
 		
 		$stmt->close();
 		$mysqli->close();
-		
-		return $array;
-		
-		
 	}
-	
-	
-	function deleteCar($id_to_be_deleted){
-		
-		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
-		
-		$stmt = $mysqli->prepare("UPDATE car_plates SET deleted=NOW() WHERE id=?");
-		$stmt->bind_param("i", $id_to_be_deleted);
-		
-		if($stmt->execute()){
-			// sai edukalt kustutatud
-			header("Location: table.php");
-			
-		}
-		
-		$stmt->close();
-		$mysqli->close();
-		
-	}
-	
-	
-	
-	//$name = "vitali";
-	//echo "Tere ".$name;
-	
-	//var_dump("asdasdasdasd");
-	
 ?>
--- table/table.php
+++ table/table.php
@@ -1,92 +1,38 @@
 <?php
-
-
-
-
-	//table php
+ 	
+ 	// table.php
 	require_once("1functions.php");
-	require_once("edit_function.php");
 	
-	
-	//kasutaja tahab midagi muuta 
-	if(isset($_POST["uptade"])){
-		
-		updateKUL($_POST["id"], $_POST["kuulutus_name"], $_POST["nimi"]);
-		
-		
-	}
-
-	//kas kasutaja tahab kustuda
-	//kas aadressireal on ?delete=???!???!!?
-	
-	if(isset($_GET["delete"])){
-		
-		//saadan kaasa id, mida kustuda
-		deleteCar($_GET["delete"]);
-		
-	}
-	
-	
-	$kuulutus_list = getKULdata();
-	//var_dump($car_list);
-
-
-?>
+	$kuulutus = getKULUTUS();
+ 
+ ?> 
+ 
 <table border=1 >
 	<tr>
-
 		<th>id</th>
-		<th>user id</th>
-		<th>kulutus nimi</th>
-		<th>nimi</th>
-		<th>X</th>
-
+		<th>name</th>
+		<th>secondname</th>
+		<th>age</th>
+		<th>eriala</th>
 	</tr>
-
-	<?php
 	
+	<?php
 	
 		// iga massiivis olema elemendi kohta
 		// count($car_list) - massiivi pikkus
-		for($i = 0; $i < count($car_list); $i++){
+		for($i = 0; $i < count($kuulutus); $i++){
 			// $i = $i +1; sama mis $i += 1; sama mis $i++;
+			echo "<tr>";
 			
-			//kui on see rida mida kasutaja tahab muuta siis kuvan input väljad
-			if(isset($_GET["edit"]) && $car_list[$i]->id == $_GET["edit"]){
-				// kasutajale muutmiseks
-				echo "<tr>";
-					echo "<form action='table.php' method='post'>";
-						echo "<td>".$car_list[$i]->id."</td>";
-						echo "<td>".$car_list[$i]->user_id."</td>";
-						echo "<input type='hidden' name ='id' value='".$car_list[$i]->id."'>";
-						echo "<td><input name='number_plate' value='".$car_list[$i]->number_plate."'></td>";
-						echo "<td><input name='color' value='".$car_list[$i]->color."'></td>";
-						echo "<td><input type='submit' name='update'></td>";
-						echo "<td><a href='table.php'>cancel</a></td>";
-					echo "</form>";
-				echo "</tr>";
-				
-			}else{
-				// tavaline rida
-				echo "<tr>";
+			echo "<td>".$kuulutus[$i]->id."</td>";
+			echo "<td>".$kuulutus[$i]->name."</td>";
+			echo "<td>".$kuulutus[$i]->secondname."</td>";
+			echo "<td>".$kuulutus[$i]->age."</td>";
+			echo "<td>".$kuulutus[$i]->eriala."</td>";
 			
-				echo "<td>".$car_list[$i]->id."</td>";
-				echo "<td>".$car_list[$i]->user_id."</td>";
-				echo "<td>".$car_list[$i]->number_plate."</td>";
-				echo "<td>".$car_list[$i]->color."</td>";
-				echo "<td><a href='?delete=".$car_list[$i]->id."'>X</a></td>";
-				echo "<td><a href='?edit=".$car_list[$i]->id."'>edit</a></td>";
-				echo "<td><a href='edit.php?edit=".$car_list[$i]->id."'>edit.php</a></td>";
-			
-				echo "</tr>";
-			}
-			
-			
+			echo "</tr>";
 		}
 	
-			
-		
-	?>	
-		
-		
+	?>
+
 </table>
