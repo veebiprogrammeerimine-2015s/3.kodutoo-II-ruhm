f07ed58e2cd99f4b2bb5f2c7169d924aa1e83324
Jarmo Habakuk <jarmhab@greeny.cs.tlu.ee>
2015-10-30 09:32:21 +0200

    update

--- page/function.php
+++ page/function.php
@@ -86,7 +86,7 @@
 		
 	$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
 	
-	$stmt = $mysqli->prepare("SELECT id, user_id, par, result FROM results"); //WHERE deleted IS NULL
+	$stmt = $mysqli->prepare("SELECT id, user_id, par, result FROM results WHERE deleted IS NULL"); 
 	$stmt->bind_result($id, $user_id, $par, $result_from_db); //algselt oli $color_from_db
 	
 	$stmt->execute();
@@ -116,6 +116,24 @@
 	$mysqli->close();
 		
 		return $array;
+}
+
+//delete funktsiooni
+
+	function deleteResult($id_to_be_deleted){
+		
+	$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+
+	$stmt = $mysqli->prepare("UPDATE results SET deleted=NOW() WHERE id=?");
+	$stmt->bind_param("i", $id_to_be_deleted);
+	if($stmt->execute()){
+			// sai edukalt kustutatud
+			header("Location: table.php");
+			$stmt->close();
+			$mysqli->close();
+		
+		}
+		
 }
 	
 	
