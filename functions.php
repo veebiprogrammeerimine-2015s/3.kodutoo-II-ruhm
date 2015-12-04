636e085701cdfb3dec26543c0532627aeb944d1d
Tatjana Kuznetsova <tatjkuz@greeny.cs.tlu.ee>
2015-12-04 08:25:13 +0200

    function uuendamine

--- functions.php
+++ functions.php
@@ -1,74 +1,65 @@
 <?php
-	//kõik AB'iga seonduv
 	
-	// ühenduse loomiseks kasuta
+	// functions.php
 	require_once("../configglobal.php");
 	$database = "if15_tanjak";
 	
-	// paneme sessiooni käima, saame kasutada $_SESSION muutujaid
-	session_start();
-	
-	// lisame kasutaja ab'i
-	function createUser($createuseremail, $password_hash, $createuserlogin, $createuseradress, $createusertelephone){
-		// globals on muutuja kõigist php failidest mis on ühendatud
-		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
-		
-		$stmt = $mysqli->prepare("INSERT INTO user_php (email, password, login, adress, telephone) VALUES (?, ?, ?, ?, ?)");
-		$stmt->bind_param("sssss", $createuseremail, $password_hash, $createuserlogin, $createuseradress, $createusertelephone);
-		$stmt->execute();
-		$stmt->close();
-		
-		$mysqli->close();		
-	}
-	
-	//logime sisse
-	function loginUser($email, $password_hash){
+	//loome uue funktsiooni, et küsida ab'ist andmeid
+	function getFlowerData(){
 		
 		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
 		
-		$stmt = $mysqli->prepare("SELECT id, email FROM user_php WHERE email=? AND password=?");
-		$stmt->bind_param("ss", $email, $password_hash);
-		$stmt->bind_result($id_from_db, $email_from_db);
+		$stmt = $mysqli->prepare("SELECT id, user_id, flower, color FROM car_plates WHERE deleted IS NULL");
+		$stmt->bind_result($id, $user_id, $flower_plate, $color_from_db);
 		$stmt->execute();
-		if($stmt->fetch()){
-			echo "kasutaja id=".$id_from_db;
+		
+		// tühi massiiv kus hoiame objekte (1 rida andmeid)
+		$array = array();
+		
+		// tee tsüklit nii mitu korda, kui saad 
+		// ab'ist ühe rea andmeid
+		while($stmt->fetch()){
 			
-			$_SESSION["id_from_db"] = $id_from_db;
-			$_SESSION["user_email"] = $email_from_db;
+			// loon objekti iga while tsükli kord
+			$car = new StdClass();
+			$car->id = $id;
+			$car->flower = $flower;
+			$car->user_id = $user_id;
+			$car->color = $color_from_db;
 			
-			//suunan kasutaja data.php lehele
-			header("Location: data.php");
+			// lisame selle massiivi
+			array_push($array, $flower);
+			//echo "<pre>";
+			//var_dump($array);
+			//echo "</pre>";
 			
-			
-		}else{
-			echo "Wrong password or email!";
 		}
-		$stmt->close();
 		
+		$stmt->close();
 		$mysqli->close();
+		
+		return $array;
+		
+		
 	}
 	
 	
-	function createFlowerColor($flower, $color){
-		// globals on muutuja kõigist php failidest mis on ühendatud
+	function deleteFlower($id_to_be_deleted){
+		
 		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
 		
-		$stmt = $mysqli->prepare("INSERT INTO car_plates (user_id, flower, color) VALUES (?, ?, ?)");
-		$stmt->bind_param("iss", $_SESSION["id_from_db"], $flower, $color);
-		
-		$message = "";
+		$stmt = $mysqli->prepare("UPDATE flowers SET deleted=NOW() WHERE id=?");
+		$stmt->bind_param("i", $id_to_be_deleted);
 		
 		if($stmt->execute()){
-			// see on tõene siis kui sisestus ab'i õnnestus
-			$message = "Edukalt sisestatud andmebaasi";
+			// sai edukalt kustutatud
+			header("Location: table.php");
 			
-		}else{
-			// execute on false, miski läks katki
-			echo $stmt->error;
 		}
 		
 		$stmt->close();
 		$mysqli->close();
-		return $message;
 		
 	}
+	
+?>
